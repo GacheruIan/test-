0def259bd1a55492905b6493361db7a5d3b2fc8e
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-08-30 23:21:41 +0300

    modify files

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,10 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
+
+
+// fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+

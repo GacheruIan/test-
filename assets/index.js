488b0b4be8b232882d1145f96ef154e12b0bbbf8
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-08-31 17:36:43 +0300

    fetch request to add images

--- assets/index.js
+++ assets/index.js
@@ -3,8 +3,26 @@ function myFunction(x) {
   }
 
 
-// fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
+  fetch("http://localhost:3000/image1/")
+  .then(res => res.json())
+ .then(function one(data){
+  const storeimg = document.querySelector('.image1').src = data[0].image0
+  
+ })
 
+
+fetch("http://localhost:3000/image2")
+.then(res => res.json())
+.then(function two(object){
+const storeimgg = document.querySelector('.image2').src = object[0].image1
+
+
+})
+
+fetch("http://localhost:3000/image3")
+.then(res => res.json())
+.then(function three(obj){
+const storeimggg = document.querySelector('.image3') .src = obj[0].image2
+console.log(obj[0].image2)
+
+})

99b21bc796dcd8225538bdc5b92834de9312792a
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-08-31 17:55:57 +0300

    modify css file

--- assets/index.js
+++ assets/index.js
@@ -26,3 +26,21 @@ const storeimggg = document.querySelector('.image3') .src = obj[0].image2
 console.log(obj[0].image2)
 
 })
+
+    // like to increment likes
+    var likes = 0;
+    var liked = false;
+    //capture input from html
+    button = document.getElementById("like-button");
+    reflect = document.getElementById("like-count");
+
+    function show() {
+    reflect.innerHTML = likes + " Likes";
+}
+    button.onclick = function() {
+
+    if (liked !== true){
+        likes++;
+    }
+    show();
+  }

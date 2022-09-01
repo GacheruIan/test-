235b788d6e0ee9ebee6f21ae8a41b3a3de87fdd9
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-09-01 21:03:13 +0300

    modify files

--- assets/index.js
+++ assets/index.js
@@ -4,11 +4,12 @@ function myFunction(x) {
 
 
   fetch("http://localhost:3000/image1/")
-  .then(res => res.json())
+ .then(res => res.json())
  .then(function one(data){
   const storeimg = document.querySelector('.image1').src = data[0].image0
   const storeText0 = document.querySelector('#h31').innerHTML = data[0].title
-  console.log(storeText0)
+  console.log(data[0].title)
+ 
 
   const addLikes = document.querySelector("#like-count");
   let likes = 0;
@@ -52,9 +53,40 @@ document.querySelector("#like-button2").addEventListener("click", () =>{
 function renderLikes() {
     document.querySelector("#like-count2").textContent = `${likes} likes`;
 }
+})
 
+
+// new ones
+
+
+fetch("http://localhost:3000/image4")
+.then(res => res.json())
+.then(function four(obj4){
+    const storeimg4 = document.querySelector('.image4').src = obj4[0].image4
+    const storeText4= document.querySelector('#h34').innerHTML = obj4[0].title
+})
+
+
+fetch("http://localhost:3000/image5")
+.then(res => res.json())
+.then(function five(obj5){
+    const storeimg5 = document.querySelector('.image5').src = obj5[0].image5
+    const storeText5= document.querySelector('#h35').innerHTML = obj5[0].title
+   
+})
+
+fetch("http://localhost:3000/image6")
+.then(res => res.json())
+.then(function five(dat){
+const image6 = document.querySelector('.image6').src = dat[0].image6
+const title = document.querySelector('#h36').innerHTML =
+console.log(title)
+    console.log(dat[0].title)
 
 })
+
+
+
 
 // feed-back form
 const myForm = document.getElementById("form").addEventListener('submit', function(event){

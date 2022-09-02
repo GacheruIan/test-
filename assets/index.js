3a53da64110c5ae1c300933b4e2debef802c7cd7
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-09-02 14:06:55 +0300

    modify files

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,7 @@ function myFunction(x) {
     x.classList.toggle("change");
   }
 
-
+  const storeimg = document.querySelector('.image1')
   fetch("http://localhost:3000/image1/")
  .then(res => res.json())
  .then(function one(data){
@@ -108,15 +108,79 @@ function renderLikes() {
    
 })
 
+let suggestions = [
+    "Tequilla-sunrise",
+    "Dry~martini.",
+    "Corpse~reviver",
+    "Margarita.",
+    "Negroni.",
+    "Espresso~martini.",
+   
+ 
+];
+// getting all required elements
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
 
+// if user press any key and release
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; //user enetered data
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = ()=>{
+            webLink = `https://www.google.com/search?q=${userData}`;
+            linkTag.setAttribute("href", webLink);
+            linkTag.click();
+        }
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = `<li>${data}</li>`;
+        });
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //hide autocomplete box
+    }
+}
 
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        webLink = `https://www.google.com/search?q=${selectData}`;
+        linkTag.setAttribute("href", webLink);
+        linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+}
 
-
-
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = `<li>${userValue}</li>`;
+    }else{
+      listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
 
 // feed-back form
 const myForm = document.getElementById("form").addEventListener('submit', function(event){
-event.preventDefault()
-event.target.reset()
-alert(`Thanks for your the feedback pal`)
-})
+    event.preventDefault()
+    event.target.reset()
+    alert(`Thanks for your the feedback pal`)
+    })
